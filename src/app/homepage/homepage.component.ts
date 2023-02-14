eacdfe412dca767541c519d006fabc0715b70699
Matthias Gutsch <matthias.gutsch@gmail.com>
2023-02-14 10:11:17 +0100

    ok

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -7,7 +7,7 @@ import { ApiService } from '../services/api.services';
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.less']
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit {
   works: Array<any>;
   products: any = [];
   references: any = [];
@@ -32,72 +32,73 @@ export class HomepageComponent {
     private titleService: Title,
     private metaService: Meta,
     ) {
-
-      this.langValue = localStorage.getItem("language");
-
-      this.responsiveOptions = [
-        {
-            breakpoint: '1024px',
-            numVisible: 3,
-            showIndicators: false,
-            showNavigators: false,
-            numScroll: 3
-        },
-        {
-            breakpoint: '768px',
-            numVisible: 2,
-            showIndicators: false,
-            numScroll: 2
-        },
-        {
-            breakpoint: '560px',
-            numVisible: 1,
-            numScroll: 1
-        }
-    ];
-
-    this.titleService.setTitle('Home');
-    this.metaService.addTag({ name: 'description', content: '' });
-    this.metaService.addTag({ name: 'robots', content: 'index,follow' });
-    this.metaService.addTag({ property: 'og:title', content: '' });
-    this.metaService.addTag({ name: 'robots', content: 'index,follow' });
-
-      const paramsProducts = this.getRequestParams(
-        this.lang = this.langValue,
-        this.categoryFilter = '220',
-        this.page,
-        this.pageSize = 6
-      );
-      this.apiService.get_all(paramsProducts).subscribe((pData) => {
-        this.products = pData;
-        this.count = this.apiService.size;
-      });
-
-      const params = this.getRequestParams(
-        this.lang =  this.langValue,
-        this.categoryFilter = '11',
-        this.page,
-        this.pageSize = 4
-      );
-      this.apiService.get_all(params).subscribe((pData) => {
-        this.works = pData;
-        this.count = this.apiService.size;
-      });
-
-      const paramsReferences = this.getRequestParams(
-        this.lang = this.langValue,
-        this.categoryFilter = '58',
-        this.page,
-        this.pageSize
-      );
-      this.apiService.get_all(paramsReferences).subscribe((pData) => {
-        this.references = pData;
-        this.count = this.apiService.size;
-      });
-
-
   }
 
+  ngOnInit(): void {
+
+
+    this.langValue = localStorage.getItem("language");
+
+    this.responsiveOptions = [
+      {
+          breakpoint: '1024px',
+          numVisible: 3,
+          showIndicators: false,
+          showNavigators: false,
+          numScroll: 3
+      },
+      {
+          breakpoint: '768px',
+          numVisible: 2,
+          showIndicators: false,
+          numScroll: 2
+      },
+      {
+          breakpoint: '560px',
+          numVisible: 1,
+          numScroll: 1
+      }
+  ];
+
+  this.titleService.setTitle('Home');
+  this.metaService.addTag({ name: 'description', content: '' });
+  this.metaService.addTag({ name: 'robots', content: 'index,follow' });
+  this.metaService.addTag({ property: 'og:title', content: '' });
+  this.metaService.addTag({ name: 'robots', content: 'index,follow' });
+
+    const paramsProducts = this.getRequestParams(
+      this.lang = this.langValue,
+      this.categoryFilter = '220',
+      this.page,
+      this.pageSize = 6
+    );
+    this.apiService.get_all(paramsProducts).subscribe((pData) => {
+      this.products = pData;
+      this.count = this.apiService.size;
+    });
+
+    const params = this.getRequestParams(
+      this.lang =  this.langValue,
+      this.categoryFilter = '11',
+      this.page,
+      this.pageSize = 4
+    );
+    this.apiService.get_all(params).subscribe((pData) => {
+      this.works = pData;
+      this.count = this.apiService.size;
+    });
+
+    const paramsReferences = this.getRequestParams(
+      this.lang = this.langValue,
+      this.categoryFilter = '58',
+      this.page,
+      this.pageSize
+    );
+    this.apiService.get_all(paramsReferences).subscribe((pData) => {
+      this.references = pData;
+      this.count = this.apiService.size;
+    });
+  }
 
   getRequestParams(langSize: string, category: string, page: number, pageSize: string | number): any {
     // tslint:disable-next-line:prefer-const
